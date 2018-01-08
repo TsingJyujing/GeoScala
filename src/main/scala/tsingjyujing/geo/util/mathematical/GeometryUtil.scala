a22ceeae39146068d61fcd16c6d45ce0ed94a8a6
袁逸凡 <yuanyifan@deewinfl.com>
2018-01-08 10:43:53 +0800

    Fix length attribute not exist in iterable

--- src/main/scala/tsingjyujing/geo/util/mathematical/GeometryUtil.scala
+++ src/main/scala/tsingjyujing/geo/util/mathematical/GeometryUtil.scala
@@ -5,11 +5,11 @@ package tsingjyujing.geo.util.mathematical
   */
 object GeometryUtil {
     
-    def angle(v1: Iterable[Double], v2: Iterable[Double]): Double = math.acos(innerProduct(v1, v2) / math.sqrt(norm2(v1) * norm2(v2))) / math.Pi * 180.00
+    def angle(v1: Array[Double], v2: Array[Double]): Double = math.acos(innerProduct(v1, v2) / math.sqrt(norm2(v1) * norm2(v2))) / math.Pi * 180.00
     
     def norm2(v: Iterable[Double]): Double = v.map(x => x * x).sum
     
-    def innerProduct(v1: Iterable[Double], v2: Iterable[Double]): Double = {
+    def innerProduct(v1: Array[Double], v2: Array[Double]): Double = {
         assert(v1.length == v2.length, "Try to get degree of two different dimension vectors.")
         (v1 zip v2).map(x => {
             x._2 * x._1
