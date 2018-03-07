3ed39ec3d2ade805bf50011dd082453dd1c5e9ba
袁逸凡 <yuanyifang@cvnavi.com>
2018-03-07 18:07:30 +0800

    Complete K-means on 2-d sphere.

--- src/main/scala/com/github/tsingjyujing/geo/algorithm/cluster/GeoKMeans.scala
+++ src/main/scala/com/github/tsingjyujing/geo/algorithm/cluster/GeoKMeans.scala
@@ -1,10 +1,12 @@
 package com.github.tsingjyujing.geo.algorithm.cluster
 
-import com.github.tsingjyujing.geo.algorithm.containers.ClusterResult
+import com.github.tsingjyujing.geo.algorithm.containers.{ClusterResult, LabeledPoint}
 import com.github.tsingjyujing.geo.basic.IGeoPoint
 import com.github.tsingjyujing.geo.util.GeoUtil
 import com.github.tsingjyujing.geo.util.mathematical.Probability
 
+import scala.collection.mutable.ArrayBuffer
+import util.control.Breaks._
 
 /**
   * Geo-K-Means++ will coming soon
@@ -19,20 +21,39 @@ object GeoKMeans {
                                  k: Int,
                                  maxStepCount: Int = 100
                              ): ClusterResult[Int, V] = {
-        val centerPoints = (1 to k).map(_ => {
-            Probability.sphereUniform
-        })
-        val lossValue = Double.MaxValue
-
-        (0 until maxStepCount).foreach(_ => {
-            points.groupBy(
-                point => {
-                    centerPoints.zipWithIndex.minBy(_._1.geoTo(point))._2
-                }
-            ).foreach(kvs => {
-                //centerPoints(kvs._1) = GeoUtil.mean(kvs._2)
+        val centerPoints = ArrayBuffer[IGeoPoint]()
+        centerPoints.appendAll(
+            (1 to k).map(_ => {
+                Probability.sphereUniform
             })
-        })
-        throw new Exception("Unimpl")
+        )
+        var lossValue = Double.MaxValue
+        breakable(
+            (0 until maxStepCount).foreach(currentStep => {
+                val EMStep1 = points.map(
+                    point => {
+                        val electedPoint = centerPoints.zipWithIndex.minBy(_._1.geoTo(point))
+                        val distance = electedPoint._1.geoTo(point)
+                        (point, distance, electedPoint._2)
+                    }
+                )
+                val currentLoss = EMStep1.map(_._2).sum
+                println("Step[%d]  Current loss=%f".format(currentStep, currentLoss))
+                if (currentLoss >= lossValue) {
+                    break()
+                } else {
+                    EMStep1.groupBy(
+                        _._3
+                    ).foreach(kvs => {
+                        centerPoints(kvs._1) = GeoUtil.mean(kvs._2.map(_._1))
+                    })
+                }
+                lossValue = currentLoss
+            })
+        )
+        ClusterResult(points.map(point => {
+            val classId = centerPoints.zipWithIndex.minBy(_._1.geoTo(point))._2
+            LabeledPoint(classId, point)
+        }))
     }
 }
