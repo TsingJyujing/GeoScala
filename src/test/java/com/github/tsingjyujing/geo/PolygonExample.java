26d0a46b96c14b746ef5ed27acc2fb1b5b2e4564
Yuan Yifan <tsingjyujing@163.com>
2018-09-15 17:34:39 +0800

    增加多边形判断的Java例程

---
+++ src/test/java/com/github/tsingjyujing/geo/PolygonExample.java
@@ -0,0 +1,32 @@
+package com.github.tsingjyujing.geo;
+
+import com.github.tsingjyujing.geo.basic.IGeoPoint;
+import com.github.tsingjyujing.geo.element.GeoPolygon;
+import com.github.tsingjyujing.geo.element.immutable.GeoPoint;
+import scala.collection.JavaConverters;
+
+import java.util.ArrayList;
+
+/**
+ * Polygon example
+ */
+public class PolygonExample {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        final GeoPolygon polygon = GeoPolygon.apply(
+                JavaConverters.asScalaIterableConverter(
+                        // Put your polygon info in this list
+                        new ArrayList<IGeoPoint>()
+                ).asScala()
+        );
+
+        boolean result = polygon.contains(
+                // Point need to query
+                GeoPoint.apply(121, 30)
+        );
+        System.out.printf("Point is %sin the polygon", result ? "" : "not ");
+    }
+}
